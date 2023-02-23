7151c3888ed9e44e2d28b9211d93f82015edc593
mruqqie <mruqqie@gmail.com>
2023-02-23 17:27:39 +0100

    modified:   client/src/App.jsx 	modified:   client/src/configs/theme.configs.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
+import { ThemeProvider } from "@mui/material/styles";
+import themeConfigs, { themeModes } from "./configs/theme.configs";
+import {useSelector} from "react-redux"
+
 const App = () => {
+  const { themeMode } = useSelector((state) => state.themeMode)
   return (
-    <div>
-      App
-    </div>
+    <ThemeProvider theme={themeConfigs.custom({ mode: themeMode})}>
+      
+    </ThemeProvider>
   );
 }
 
