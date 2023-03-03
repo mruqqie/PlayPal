a41dfed32f77d51f9b5e2469f8b57f701b5db99a
mruqqie <mruqqie@gmail.com>
2023-03-03 17:26:23 +0100

    modified:   server/.env 	modified:   server/src/middleware/token.middleware.js

--- server/src/middleware/token.middleware.js
+++ server/src/middleware/token.middleware.js
@@ -3,39 +3,36 @@ import resHandler from "../handlers/res.handler.js";
 import userModel from "../models/user.model.js";
 
 const tokenDecode = (req) => {
-    try {
-        const bearerHeader = req.headers["authorization"]
+	try {
+		const bearerHeader = req.headers["authorization"];
 
-        if (bearerHeader) {
-            const token = bearerHeader.split(" ")[1]
+		if (bearerHeader) {
+			const token = bearerHeader.split(" ")[1];
 
-            return jsonwebtoken.verify(
-                token,
-                process.env.TOKEN_SECRET
-            )
-        }
-        return false
-    } catch {
-        return false;
-    }
+			return jsonwebtoken.verify(token, process.env.TOKEN_SECRET);
+		}
+		return false;
+	} catch {
+		return false;
+	}
 };
 
-const auth = async (res, req, next) => {
-    const tokenDecoded = tokenDecode(req);
+const auth = async (req, res, next) => {
+	const tokenDecoded = tokenDecode(req);
 
-    if (!tokenDecode) {
-        return resHandler.unauthorized(res)
-    }
+	if (!tokenDecoded) {
+		return resHandler.unauthorized(res);
+	}
 
-    const user = await userModel.findById(tokenDecoded.data);
+	const user = await userModel.findById(tokenDecoded.data);
 
-    if (!user) {
-        return resHandler.unauthorized(res);
-    };
+	if (!user) {
+		return resHandler.unauthorized(res);
+	}
 
-    req.user = user;
+	req.user = user;
 
-    next();
+	next();
 };
 
 export default { auth, tokenDecode };
