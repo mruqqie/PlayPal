87ecf9aa6f42e14ad58b7ab8ec1ef5ccf02c4223
mruqqie <mruqqie@gmail.com>
2023-03-04 10:50:06 +0100

    modified:   server/src/controllers/user.controller.js 	modified:   server/src/tmdb/tmdb.api.js

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -3,118 +3,110 @@ import jsonwebtoken from "jsonwebtoken";
 import resHandler from "../handlers/res.handler.js";
 
 const signup = async (req, res) => {
-    try {
-        const { username, password, displayName } = req.body;
+	try {
+		const { username, password, displayName } = req.body;
 
-        const checkUser = await userModel.findOne({ username });
-        if (checkUser) {
-            return resHandler.badReq(res, "Username already in use.");
-        };
+		const checkUser = await userModel.findOne({ username });
+		if (checkUser) {
+			return resHandler.badReq(res, "Username already in use.");
+		}
 
-        const user = new userModel();
+		const user = new userModel();
 
-        user.displayName = displayName;
-        user.username = username;
-        user.setPassword(password);
-        await user.save();
+		user.displayName = displayName;
+		user.username = username;
+		user.setPassword(password);
+		await user.save();
 
-        const payload = { data: user.id };
-        const options = { expiresIn: "24h" };
-        const token = jsonwebtoken.sign(
-            payload,
-            process.env.TOKEN_SECRET,
-            options
-        );
+		const payload = { data: user.id };
+		const options = { expiresIn: "24h" };
+		const token = jsonwebtoken.sign(payload, process.env.TOKEN_SECRET, options);
 
-        resHandler.created(res, {
-            token,
-            ...user._doc,
-            id: user.id
-        });
-    } catch {
-        resHandler.error(res);
-    }
+		resHandler.created(res, {
+			token,
+			...user._doc,
+			id: user.id,
+		});
+	} catch {
+		resHandler.error(res);
+	}
 };
 
 const signin = async (req, res) => {
-    try {
-        const { username, password } = req.body;
+	try {
+		const { username, password } = req.body;
 
-        const user = await userModel.findOne({ username }).select(
-            "username password salt id displayName"
-        );
+		const user = await userModel
+			.findOne({ username })
+			.select("username password salt id displayName");
 
-        if (!user) {
-            return resHandler.badReq(res, "User not found.")
-        };
+		if (!user) {
+			return resHandler.badReq(res, "User not found.");
+		}
 
-        if (!user.validPassword(password)) {
-            return resHandler.badReq(res, "Wrong password")
-        }
+		if (!user.validPassword(password)) {
+			return resHandler.badReq(res, "Wrong password");
+		}
 
-        const payload = { data: user.id };
-        const options = { expiresIn: "24h" };
-        const token = jsonwebtoken.sign(
-            payload,
-            process.env.TOKEN_SECRET,
-            options
-        );
+		const payload = { data: user.id };
+		const options = { expiresIn: "24h" };
+		const token = jsonwebtoken.sign(payload, process.env.TOKEN_SECRET, options);
 
-        user.password = undefined;
-        user.salt = undefined;
+		user.password = undefined;
+		user.salt = undefined;
 
-        resHandler.created(res, {
-            token,
-            ...user._doc,
-            id: user.id
-        });
-    } catch {
-        resHandler.error(res)
-    }
+		resHandler.created(res, {
+			token,
+			...user._doc,
+			id: user.id,
+		});
+	} catch {
+		resHandler.error(res);
+	}
 };
 
 const updatePassword = async (req, res) => {
-    try {
-        const { password, newPassword } = req.body;
+	try {
+		const { password, newPassword } = req.body;
 
-        const user = await userModel.findById(req.user.id).select(
-            "password id salt"
-        );
+		const user = await userModel
+			.findById(req.user.id)
+			.select("password id salt");
 
-        if (!user) {
-            return resHandler.unauthorized(res)
-        };
+		if (!user) {
+			return resHandler.unauthorized(res);
+		}
 
-        if (!user.validPassword(password)) {
-            return resHandler.badReq(res, "Wrong password.")
-        };
+		if (!user.validPassword(password)) {
+			return resHandler.badReq(res, "Wrong password.");
+		}
 
-        user.setPassword(newPassword)
-        await user.save();
+		user.setPassword(newPassword);
+		await user.save();
 
-        resHandler.ok(res);
-    } catch  {
-        resHandler.error(res);
-    }
+		resHandler.ok(res);
+	} catch {
+		resHandler.error(res);
+	}
 };
 
 const getInfo = async (req, res) => {
-    try {
-        const user = await userModel.findById(req.user.id)
+	try {
+		const user = await userModel.findById(req.user.id);
 
-        if (!user) {
-            resHandler.notFound(res)
-        };
+		if (!user) {
+			resHandler.notFound(res);
+		}
 
-        resHandler.ok(res, user);
-    } catch  {
-        resHandler.error(res);
-    }
+		resHandler.ok(res, user);
+	} catch {
+		resHandler.error(res);
+	}
 };
 
 export default {
-    signup,
-    signin,
-    updatePassword,
-    getInfo
-}
+	signup,
+	signin,
+	updatePassword,
+	getInfo,
+};
